c30711bf216701683eade93a7e09b49120ad5cbc
Xiangxu Teng <tengxxu@gmail.com>
2021-04-30 18:51:22 -0400

    Initial commit

---
+++ Buzzerr/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buzzerr.Models
+{
+    public class Student
+    {
+        [Key]
+        [HiddenInput]
+        public int StudentID { get; set; }
+
+        [Required]
+        public String FirstName { get; set; }
+
+        [Required]
+        public String LastName { get; set; }
+
+        [EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+(@utoronto\.ca|@uwo\.ca|@yorku.ca)$", ErrorMessage = "Registration limited to utoronto.ca, uwo.ca and yorku.ca.")]
+        [Required]
+        public string UserEmail { get; set; }
+
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+
+    }
+}
---
+++ Buzzerr/Pages/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buzzerr.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly Buzzerr.Context.DataContext _context;
+
+        public IndexModel(Buzzerr.Context.DataContext context)
+        {
+            _context = context;
+        }
+
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public void OnGet()
+        {
+
+        }
+
+        [BindProperty]
+        public Models.Student Student { get; set; }
+
+
+        public async Task<IActionResult> OnPostAsync()
+
+        {
+            if (!ModelState.IsValid)
+
+            {
+                return Page();
+
+            }
+            else {
+                _context.Students.Add(Student);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+        }
+
+    }
+}
